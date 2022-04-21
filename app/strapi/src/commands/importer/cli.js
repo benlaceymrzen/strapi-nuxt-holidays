597a4614d4bef7b20ed3836829665aa3bb0a79c8
Ben Lacey <ben.lacey@mrzen.com>
2022-04-21 16:01:37 +0100

    Committing progress on the importer. It checks if database backup has been done, asks you to select the file from a list of choices and then parses the data in the file after checking that it exists

--- app/strapi/src/commands/importer/cli.js
+++ app/strapi/src/commands/importer/cli.js
@@ -1,8 +1,46 @@
 // const strapi = require('strapi')
 const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+const dataDirectoryPath = path.join(__dirname, '../../../data/');
 
 // Show welcome banner
-require('./src/welcome.js');
+console.log("")
+console.log("")
+console.log("******************************")
+console.log("**  Custom Strapi Importer  **")
+console.log("******************************")
+console.log("")
+
+// List data files that can be selected to import
+function listImportFiles(){
+  fs.readdir(dataDirectoryPath, function (err, filenames) {
+    if (err) { return console.log('Unable to scan directory: ' + err); }
+    let files = []
+    let index = 1;
+
+    if(!err) {
+      filenames.forEach(file => {
+        // console.log(file)
+        files.push(
+          {
+            key: index,
+            value: file
+          }
+        )
+
+        index++
+      });
+      // console.log("")
+    }
+
+    //files = filenames.join(", ")
+    // console.log(files)
+    return files
+  });
+}
+
 
 inquirer.prompt([
     {
@@ -19,19 +57,47 @@ inquirer.prompt([
       console.log("Please make a backup before proceeding");
 
     } else {
+      let choices_list = [
+        {key: 'a', value:'Facilities_enV4.json'},
+        {key: 'b', value:'LocationsV4.json'},
+        {key: 'c', value:'ProvincesV4.json'}
+      ]
 
       // Prompt for the filename to import into Strapi
       inquirer.prompt([
         {
+          type: 'expand',
           name: 'filenamePrompt',
-          message: 'Please enter the filename to import: '
+          message: 'Select files to import: ',
+          choices: choices_list
         },
       ])
         .then(answers => {
           const filenamePromptAnswer = answers.filenamePrompt;
-          console.info('Filename: ', filenamePromptAnswer);
+          let json_file_path = dataDirectoryPath + filenamePromptAnswer
+
+          console.log(filenamePromptAnswer)
+          console.info('Filename: ', json_file_path)
+
+          // Process File
+          console.log("Processing File: ", answers.filenamePrompt)
+
+          try {
+            if (fs.existsSync(json_file_path)) {
+              // file exists
+              fs.readFile(json_file_path, (err, data) => {
+                if (err) throw err;
+                let parsedData = JSON.parse(data);
+                console.log(parsedData);
+              });
+            }
+          } catch(err) {
+            console.error("Error reading file - ", err)
+          }
+
         }
       );
+
     }
   }
 );
--- app/strapi/src/commands/importer/src/welcome.js
+++
@@ -1,7 +0,0 @@
-
-console.log("")
-console.log("")
-console.log("******************************")
-console.log("**  Custom Strapi Importer  **")
-console.log("******************************")
-console.log("")
