e8bacd8436827cae635c755789ca4422de02080e
Ben Lacey <ben.lacey@mrzen.com>
2022-04-28 11:05:14 +0100

    Removed the content types that have been converted into components

--- app/strapi/src/api/room-type/controllers/room-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  room-type controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::room-type.room-type');
--- app/strapi/src/api/room-type/services/room-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * room-type service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::room-type.room-type');
